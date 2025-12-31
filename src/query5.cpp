9f4cfc0fb6709f2b43c5f61775f6a439d51626af
Manish Kumar <mk4014104@gmail.com>
2025-12-31 11:04:44 +0530

    MultiThreaded Query5

--- src/query5.cpp
+++ src/query5.cpp
@@ -5,7 +5,7 @@
 #include <thread>
 #include <mutex>
 #include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
 // Function to parse command line arguments
 bool parseArgs(int argc, char* argv[], std::string& r_name, std::string& start_date, std::string& end_date, int& num_threads, std::string& table_path, std::string& result_path) {
@@ -85,11 +85,86 @@ bool readTPCHData(const std::string& table_path, std::vector<std::map<std::strin
 	    loadhelper(table_path + "/nation.tbl", {"n_nationkey", "n_name", "n_regionkey"}, nation_data) &&
 	    loadhelper(table_path + "/region.tbl", {"r_regionkey", "r_name"}, region_data);
 }
+//Worker-utility Function
+static void worker(int start, int end, const vector<map<string,string>>& lineitem_data, const unordered_set<int>& valid_suppliers, const unordered_map<int,int>& order_to_nation, unordered_map<int,double>& local_result)
+{
+	for (int i = start; i < end; i++) {
+		const auto& l = lineitem_data[i];
+		
+		int suppkey = stoi(l.at("l_suppkey"));
+		if (!valid_suppliers.count(suppkey))
+			continue;
+		
+		int orderkey = std::stoi(l.at("l_orderkey"));
+		auto it = order_to_nation.find(orderkey);
+		
+		if (it == order_to_nation.end())
+			continue;
+		
+		double price = stod(l.at("l_extendedprice"));
+		double discount = stod(l.at("l_discount"));
+		
+		local_result[it->second] += price * (1.0 - discount);
+	}
+}
+
+
 
 // Function to execute TPCH Query 5 using multithreading
 bool executeQuery5(const std::string& r_name, const std::string& start_date, const std::string& end_date, int num_threads, const std::vector<std::map<std::string, std::string>>& customer_data, const std::vector<std::map<std::string, std::string>>& orders_data, const std::vector<std::map<std::string, std::string>>& lineitem_data, const std::vector<std::map<std::string, std::string>>& supplier_data, const std::vector<std::map<std::string, std::string>>& nation_data, const std::vector<std::map<std::string, std::string>>& region_data, std::map<std::string, double>& results) {
     // TODO: Implement TPCH Query 5 using multithreading
-    return false;
+    int regionkey = -1;
+    for (const auto& r : region_data)
+	    if (r.at("r_name") == r_name)
+		    regionkey = stoi(r.at("r_regionkey"));
+    
+    unordered_map<int,string> nation_name;
+    unordered_set<int> valid_nations;
+    for (const auto& n : nation_data)
+	    if (stoi(n.at("n_regionkey")) == regionkey) {
+		    int nk = stoi(n.at("n_nationkey"));
+		    valid_nations.insert(nk);
+		    nation_name[nk] = n.at("n_name");
+	    }
+    
+    unordered_set<int> valid_suppliers;
+    for (const auto& s : supplier_data)
+	    if (valid_nations.count(stoi(s.at("s_nationkey"))))
+		    valid_suppliers.insert(stoi(s.at("s_suppkey")));
+    
+    unordered_map<int,int> customer_to_nation;
+    for (const auto& c : customer_data)
+	    if (valid_nations.count(stoi(c.at("c_nationkey"))))
+		    customer_to_nation[stoi(c.at("c_custkey"))] =stoi(c.at("c_nationkey"));
+    
+    unordered_map<int,int> order_to_nation;
+    for (const auto& o : orders_data) {
+	    if (o.at("o_orderdate") >= start_date && o.at("o_orderdate") < end_date) {
+		    int custkey = stoi(o.at("o_custkey"));
+		    if (customer_to_nation.count(custkey))
+			    order_to_nation[stoi(o.at("o_orderkey"))] = customer_to_nation[custkey];
+	    }
+    }
+    
+    vector<thread> threads;
+    vector<unordered_map<int,double>> partials(num_threads);
+    
+    int chunk = lineitem_data.size() / num_threads;
+    
+    for (int i = 0; i < num_threads; i++) {
+	    
+	    int start = i * chunk;
+	    int end = (i == num_threads - 1) ? lineitem_data.size(): start + chunk;
+	    threads.emplace_back(worker, start, end, cref(lineitem_data), cref(valid_suppliers), cref(order_to_nation), ref(partials[i]));
+    }
+    
+    for (auto& t : threads)
+	    t.join();
+    
+    for (auto& mp : partials)
+	    for (auto& [nk, rev] : mp)
+		    results[nation_name[nk]] += rev;
+    return true;
 }
 
 // Function to output results to the specified path
